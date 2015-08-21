eb603acd01ec5c1215391937433667a2bab41618
Brian Millar <amb3r@protonmail.ch>
2015-08-21 12:45:53 +0100

    Updated Folder Structure

---
+++ sys/libs/exastum.js
@@ -0,0 +1,186 @@
+/*	Copyright 2015 Brian Millar
+	This file is part of eXastum.
+	eXastum is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	eXastum is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with eXastum.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+var buildNo  = "3.0.0.2";
+var codeName = "Silverfish - Alpha";
+var language = "english";
+var skin     = lStore("skin");
+
+function init() {
+	consoleWrite("eXastum build " + buildNo + " - " + codeName);
+	if(!checkSetup()) startSetup();
+	loadStrings(language);
+	loadSettings();
+	initGUI();
+	clock();
+}
+
+function checkSetup() {
+	if (lStore("setup") != null) return true;
+	return false;
+}
+
+function clock() {
+	$("sysClock").innerHTML = fDate() + "  -  " + fTime();
+	setTimeout("clock()",500);
+}
+
+function updateInfoBar(text) {
+	$("infoBar").value = text;
+}
+
+function startSetup() {
+	consoleWrite("Setup is not compelete, starting setup...");
+}
+
+function loadStrings(lang) {
+}
+
+function loadTheme() {
+	$("Sheet").href     = "sys/skins/" + skin + "/main.css";
+	$("bootsplash").src = "sys/skins/" + skin + "/ui/bootsplash.jpg";
+	$("wallpaper").src  = "sys/skins/" + skin + "/wallpaper/default.jpg";
+}
+
+function changeTheme(x) {
+	lStore("skin",x);
+	loadTheme();
+}
+
+function showAbout() {
+	sysDialog("About eXastum","eXastum build " + buildNo + " - " + codeName,false);
+}
+
+function loadSettings() {
+
+}
+
+function installApp(appData) {
+	lStore();
+}
+
+function launchApp(app) {
+	newWindow(lStore(app));
+}
+
+function sysDialog(error,message,urgent) {
+	var dialog = newWindow(400,50,error,null,"none",false,false);
+	var msg    = generate("span",dialog);
+	$(msg).innerHTML = message;
+	$(msg).setAttribute("class","sysDialog");
+	if(urgent)
+		$(dialog).setAttribute("class","sysDialogUrgent");
+	else
+		$(dialog).setAttribute("class","sysDialog");
+}
+
+function showDock() {
+	$("dock").style.MozTransform    = "translateY(0px)";
+	$("dock").style.transform       = "translateY(0px)";
+	$("dock").style.webkitTransform = "translateY(0px)";
+	$("dock").style.msTransform     = "translateY(0px)";
+	$("dock").style.OTransform      = "translateY(0px)";
+}
+
+function hideDock() {
+	$("dock").style.MozTransform    = "translateY(140px)";
+	$("dock").style.transform       = "translateY(140px)";
+	$("dock").style.webkitTransform = "translateY(140px)";
+	$("dock").style.msTransform     = "translateY(140px)";
+	$("dock").style.OTransform      = "translateY(140px)";
+}
+
+function initGUI() {
+	$("buildNum").innerHTML       = "eXastum " + buildNo + "<br/>" + codeName;
+	$("bootsplash").style.display = "none";
+	$("login").style.display      = "block";
+	loadTheme();
+}
+
+function clickDesktop() {
+	$("mainMenu").style.display = "none";
+	$("menuButton").src         = "sys/skins/" + lStore("skin") + "/ui/menu.png";
+	updateInfoBar("eXastum 3.0");
+}
+
+function loadUiColors() {
+	$("styleSystem").innerHTML = "#buildNum {color:" + uiColor + ";}" + "#bar {color:" + uiColor + ";}" + "#barUnderlay {background-color:" + uiColor + "; border-color:" + uiColor + ";}" + "button {color:" + uiColor + "; border-color:" + uiColor + ";}" + "div.window {border-color:" + uiColor + ";}" + "div.window:hover {box-shadow:0px 0px 5px " + uiColor + ";}" + "@-moz-keyframes window {0% {opacity:0.0; box-shadow:0px 0px 400px 50px " + uiColor + "; -moz-transform: scale(0.0) translateY(150px) rotateY(45deg) rotateX(45deg);} 100% {opacity:1.0;-moz-transform:scale(1.0) translateY(0px) rotateY(0deg) rotateX(0deg);}}";
+}
+
+function updateUiColor(newColor) {
+	lStore("uiColor",newColor);
+	uiColor = lStore("uiColor");
+	loadUiColors();
+}
+
+function newConsole() {
+	var console = newWindow(450,350,"Console",null,"both",true,true);
+	var cli     = generate("textarea",console);
+	$(cli).setAttribute("class","cli");
+	$(cli).setAttribute("spellcheck","false");
+	$(cli).setAttribute("placeholder","eXastum Studio v1.0 - JavaScript Console>_");
+	consoleInit(cli);
+}
+
+function consoleInit(txtArea) {
+	$(txtArea).setAttribute("onkeypress","onStrikeEnter(this.value,event,false)");
+	$(txtArea).focus();
+}
+
+function consoleWrite(message) {
+	$("console").innerHTML = $("console").innerHTML + message + "\n";
+	log(message);
+}
+
+function showHideMainMenu() {
+	if ($("mainMenu").style.display == "none") {
+		$("mainMenu").style.display =  "block";
+		$("menuButton").src = "sys/skins/" + lStore("skin") + "/ui/menuPress.png";
+	}
+	else {
+		$("menuButton").src         = "sys/skins/" + lStore("skin") + "/ui/menu.png";
+		$("mainMenu").style.display = "none";
+	}
+}
+
+function setTheme(text, color, opacity) {
+	if (text == "default") {
+		lStore("themeText",    "#e3e3e3");
+		lStore("themeColor",   "0,0,0");
+		lStore("themeOpacity", "0.8");
+	}
+	else {
+		lStore("themeText",    text);
+		lStore("themeColor",   color);
+		lStore("themeOpacity", opacity);
+	}
+	loadTheme();
+}
+
+function loadTheme() {
+	var themeText    = lStore("themeText");
+	var themeColor   = lStore("themeColor");
+	var themeOpacity = lStore("themeOpacity");
+	var currentStyle = $("styleSystem").innerHTML;
+	var themeValues  = "rgba(" + themeColor + ", " + themeOpacity + ")";
+	document.body.style.color   = themeText;
+	$("styleSystem").innerHTML  = currentStyle + "button {background-color: " + themeValues + "; color: " + themeText + ";}";
+	$("mainMenu").style.display = "none";
+}
+
+function login() {
+	$("login").style.display    = "none";
+	$("loggedIn").style.display = "block";
+	lStore("skin","default");
+}
---
+++ sys/libs/macgril.js
@@ -0,0 +1,153 @@
+/*	Copyright 2015 Brian Millar
+	This file is part of eXastum.
+	eXastum is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	eXastum is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with eXastum.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+var mgID = 0;
+
+function macgrilInit() {
+	$("macgril").style.display = "none";
+}
+
+function print(x) {
+	document.write(x);
+}
+
+function log(x) {
+	console.log(x);
+}
+
+
+//DOM Functions
+
+function $(element) {
+	var id = document.getElementById(element);
+	if ((id != null) && (id != undefined)) return id;
+}
+
+function generate(x,y,z) {
+	var el = document.createElement(x);
+	var id;
+	if (z != null) id = z;
+	else id = "macgrilID" + (mgID++);
+	el.setAttribute("id",id);
+	$(y).appendChild(el);
+	return id;
+}
+
+
+//File Access & Storage
+
+function lStore(x,y) {
+	if ((y != null) && (y != "del"))
+		window.localStorage.setItem(x,y);
+	else if (y == "del")
+		window.localStorage.removeItem(x);
+	else
+		return window.localStorage.getItem(x);
+}
+
+function fOpen(accept) {
+	if (($("fOpen") != 0) && ($("fOpen") != undefined))
+		$("macgril").removeChild($("fOpen"));
+	generate("input","macgril","fOpen");
+	$("fOpen").setAttribute("type","file");
+	if ((accept != null) && (accept != undefined))
+		$("fOpen").setAttribute("accept", accept);
+	$("fOpen").click();
+	var x = window.confirm("Are you sure you want to load this file from disk?");
+	if (x) if ($('fOpen').files[0] != undefined)
+			return window.URL.createObjectURL($('fOpen').files[0]);
+}
+
+
+//Date & Time Functions
+
+var mgDate = new Date();
+
+function fDate() {
+	mgDate    = new Date();
+	var day   = mgDate.getDate();
+	var month = mgDate.getMonth() + 1;
+	var year  = mgDate.getFullYear().toString();
+	if (day   < 10) day   = "0"+day;
+	if (month < 10) month = "0"+month;
+	return day + "/" + month + "/" + year.charAt(2) + year.charAt(3);
+}
+
+function fTime() {
+	mgDate      = new Date();
+	var hours   = mgDate.getHours();
+	var minutes = mgDate.getMinutes();
+	var seconds = mgDate.getSeconds();
+	if (hours < 10)   hours   = "0" + hours;
+	if (minutes < 10) minutes = "0" + minutes;
+	if (seconds < 10) seconds = "0" + seconds;
+	return hours + ":" + minutes + ":" + seconds;
+}
+
+
+//Input Functions
+
+function onStrikeEnter(func,ev,prvnt) {
+	if ((ev.which === 13) || (ev.keyCode === 13)) {
+		if (prvnt) ev.preventDefault();
+		return (eval(func));
+	}
+}
+
+function knobLogic(knob,ev,func) {
+	var startPos    = ev.clientY;
+	var newPos      = startPos;
+	var startingRot = knob.style.MozTransform;
+	var startRot    = "";
+	for (var i = 0; i < startingRot.length; i++)
+		if (!isNaN(startingRot[i]))
+			startRot = startRot+startingRot[i];
+	document.onmousemove = function(ev) {
+		var rotation = parseInt(startRot);
+		newPos       = rotation + parseInt(startPos - ev.clientY);
+		if (newPos > 120)  newPos = 120;
+		if (newPos < -120) newPos = -120;
+		knob.style.MozTransform   = "rotate(" + newPos + "deg)";
+		rotation = 0;
+		if ((func != null) && (func != undefined)) eval(func);
+		document.onmouseup = function(ev) {
+			newPos   = null;
+			startPos = null;
+			document.onmousemove = null;
+			document.onmouseup   = null;
+			return newPos;
+		}
+	}
+}
+
+function switchTabs(id1,id2) {
+	$(id1).style.display = "none";
+	$(id2).style.display = "block";
+}
+
+function showHideIDs(idArray,x) {
+	for (var i = 0; i < idArray.length; i++) {
+		if (x == "show")
+			$(idArray[i]).style.display = "block";
+		else
+			$(idArray[i]).style.display = "none";
+	}
+}
+
+
+//Mathematics Functions
+
+function randNum(x,y) {
+	return (Math.random() * x).toFixed(y);
+}
